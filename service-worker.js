12445e851bc8bbbcbaf553de2611217f8bbf702e
juneryy <155787613+juneryy@users.noreply.github.com>
2026-04-08 11:45:41 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = "net-hours-v2";
+const CACHE_NAME = "net-hours-v3";
+
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
@@ -26,4 +28,17 @@ self.addEventListener("activate", event => {
     )
   );
   self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(response => response || caches.match("./index.html"));
+      })
+  );
 });
